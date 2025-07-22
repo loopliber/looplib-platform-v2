04e7cbfdf732fddb22645fcbb64450f762742a5b
loopliber <looplib1@gmail.com>
2025-07-22 15:29:46 +0200

    ebook

--- src/components/GenrePageTemplate.tsx
+++ src/components/GenrePageTemplate.tsx
@@ -288,7 +288,7 @@ export default function GenrePageTemplate({ config, initialSamples = [] }: Genre
       const extension = sample.file_url.split('.').pop() || 'mp3';
       const keyFormatted = sample.key ? sample.key.toLowerCase().replace(/\s+/g, '') : 'cmaj';
       const nameFormatted = sample.name.toLowerCase().replace(/\s+/g, '');
-      const downloadFilename = `${nameFormatted}_${sample.bpm}_${keyFormatted} @LOOPLIB.${extension}`;
+      const downloadFilename = `${nameFormatted}_${sample.bpm}_${keyFormatted} @ajcookin.${extension}`;
 
       await downloadFile(sample.file_url, downloadFilename);
       toast.success('Download complete!');
